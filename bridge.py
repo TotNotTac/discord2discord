e2c99ad5d65362379ceff4c4b185a7d1ee363ab3
Simonx22 <Simonx22@users.noreply.github.com>
2017-06-09 15:03:09 +0200

    Some cleanup, it's a channel<--> channel bridge now

--- bridge.py
+++ bridge.py
@@ -13,8 +13,11 @@ client = discord.Client()
 async def on_message(message):
 	if message.author == client.user:
 		return
-	log = message.content
-	await client.send_message(discord.Object(id='Channel_ID'), log)
+	if message.channel.id == 'Channel:_ID1':
+		await client.send_message(discord.Object(id='Channel_ID2'), message.content)
+		
+	if message.channel.id == 'Channel_ID2':
+		await client.send_message(discord.Object(id='Channel_ID1'), message.content)
 	
 @client.event
 async def on_ready():
